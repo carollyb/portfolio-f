4ffeb1d7b894b6396bcf0d47f8c8c159a1051793
carolly <carolly.barbosa@gmail.com>
2025-02-22 18:33:18 -0300

    chore: deletes unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,11 @@ import { Footer } from '@/frontend/modules/footer/ui/Footer';
 import { Header } from '@/frontend/modules/header/ui/Header';
 import { Projects } from '@/frontend/modules/projects/ui/Projects';
 import { SplashScreen } from '@/frontend/modules/splash/ui/SplashScreen';
-import { useColorScheme } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
   const [tag, setTag] = useState<string>('all');
-  const { mode, setMode } = useColorScheme();
-
-  useEffect(() => {
-    console.log(mode);
-  }, []);
 
   return (
     <>
